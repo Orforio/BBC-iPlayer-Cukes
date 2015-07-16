011de5d69c2856ba42f70c958479ea7823ffbeb3
Richard Whittaker <pkeruno@gmail.com>
2015-07-16 23:18:21 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+group :test do
+  gem 'cucumber'
+  gem 'rspec-expectations'
+  gem 'selenium-webdriver'
+  gem 'poltergeist'
+  gem 'capybara'
+end
---
+++ cukes/features/support/env.rb
@@ -0,0 +1,20 @@
+require 'rspec/expectations'
+require 'capybara/cucumber'
+require 'capybara/poltergeist'
+
+if ENV['IN_BROWSER']
+  Capybara.default_driver = :selenium
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :firefox)
+  end
+else
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, window_size: [1280, 1024], timeout: 60)
+  end
+  Capybara.default_driver = :poltergeist
+  Capybara.javascript_driver = :poltergeist
+end
+
+Capybara.app_host = 'http://www.bbc.co.uk'
+
+World(Rspec::Matchers)
