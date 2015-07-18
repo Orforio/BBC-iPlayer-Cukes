5fbdc66495792c5d966b3291c3b2558b0afb7942
Richard Whittaker <pkeruno@gmail.com>
2015-07-18 03:09:03 +0100

    Change from Poltergeist to Selenium

--- cukes/features/support/env.rb
+++ cukes/features/support/env.rb
@@ -1,18 +1,19 @@
 require 'rspec/expectations'
 require 'capybara/cucumber'
 require 'capybara/poltergeist'
+require 'selenium-webdriver'
 
-if ENV['IN_BROWSER']
-  Capybara.default_driver = :selenium
-  Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(app, :browser => :firefox)
-  end
-else
+if ENV['PHANTOM']
   Capybara.register_driver :poltergeist do |app|
-    Capybara::Poltergeist::Driver.new(app, window_size: [1280, 1024], timeout: 60)
+    Capybara::Poltergeist::Driver.new(app, window_size: [1280, 1024], timeout: 60, :js_errors => false, :debug => false)
   end
   Capybara.default_driver = :poltergeist
   Capybara.javascript_driver = :poltergeist
+else
+  Capybara.default_driver = :selenium
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  end
 end
 
 Capybara.app_host = 'http://www.bbc.co.uk'
